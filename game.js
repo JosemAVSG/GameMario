3644aa1f5b751543b31d3c55c9acfc2b914de101
José Miguel González Guzmán <107879363+JosemAVSG@users.noreply.github.com>
2024-08-11 15:20:08 -0500

    assets

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ const config = {
     type: Phaser.AUTO,
     width: 256,
     height: 244,
-    backgroundColor: 'white',
+    backgroundColor:'#049cd8',
     parent: 'game',
     // physics: {
     //     default: 'arcade',
@@ -14,20 +14,51 @@ const config = {
     //     }
     // },
     scene: {
-        preload
+        preload,
         create,
         update,
     }
 };
 
+new Phaser.Game(config);
+
 function preload() {
-    this.load.image('mario', 'mario.png');
+    this.load.image('cloud1','assets/scenery/overworld/cloud1.png');
+
+    this.load.image('floorbrick','assets/scenery/overworld/floorbricks.png');
+
+    this.load.spritesheet('mario', 'assets/entities/mario.png', {
+        frameWidth: 18,
+        frameHeight: 16
+    })
 }
 
 function create() {
-    this.add.image(0, 0, 'mario').setOrigin(0, 0);
+    this.add.image(150, 50, 'cloud1').setOrigin(0 , 0).setScale(0.15);
+
+    this.add.tileSprite(0, config.height, config.width, 32, 'floorbrick').setOrigin(0 , 1);
+
+    this.mario=this.add.sprite(100, 190, 'mario').setOrigin(0 , 0).setScale(1.5);
+    this.keys = this.input.keyboard.createCursorKeys();
+
+    this.anims.create({
+        key:'mario-walk',
+        frames: this.anims.generateFrameNumbers('mario', {start: 0, end: 3}),
+        frameRate: 10,
+        repeat: -1
+    })
+
 }
 
 function update() {
-    
+    if(this.keys.left.isDown) {
+        this.mario.x -=2;
+        this.mario.anims.play('mario-walk' );
+    }else if(this.keys.right.isDown) {
+        this.mario.x += 2;
+        this.mario.anims.play('mario-walk');
+    }else{
+        this.mario.anims.stop();
+        this.mario.frame = 0;
+    }
 }
